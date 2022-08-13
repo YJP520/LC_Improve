85238ceb28ae2a982d71b26a62071958aee4e0ef
YJP520 <76167345+YJP520@users.noreply.github.com>
2022-08-14 00:35:47 +0800

    Initial commit

---
+++ src/0813_LC.py
@@ -0,0 +1,62 @@
+#
+# Time : 2022/08/13
+# Author : YU.J.P
+# Details : LeetCode 算法程序设计
+#
+
+# import random
+# import turtle
+# import time
+# import math
+# import this  # print(this.v)
+# import copy
+# import string
+
+# T5 给你一个字符串 s，找到 s 中最长的回文子串。
+
+import string
+
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        n = len(s)
+        if n < 2:
+            return s
+
+        max_len = 1
+        begin = 0
+        # dp[i][j] 表示 s[i..j] 是否是回文串
+        dp = [[False] * n for _ in range(n)]
+        for i in range(n):
+            dp[i][i] = True
+
+        # 递推开始
+        # 先枚举子串长度
+        for L in range(2, n + 1):
+            # 枚举左边界，左边界的上限设置可以宽松一些
+            for i in range(n):
+                # 由 L 和 i 可以确定右边界，即 j - i + 1 = L 得
+                j = L + i - 1
+                # 如果右边界越界，就可以退出当前循环
+                if j >= n:
+                    break
+
+                if s[i] != s[j]:
+                    dp[i][j] = False
+                else:
+                    if j - i < 3:
+                        dp[i][j] = True
+                    else:
+                        dp[i][j] = dp[i + 1][j - 1]
+
+                # 只要 dp[i][L] == true 成立，就表示子串 s[i..L] 是回文，此时记录回文长度和起始位置
+                if dp[i][j] and j - i + 1 > max_len:
+                    max_len = j - i + 1
+                    begin = i
+        return s[begin:begin + max_len]
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    solution = Solution()
+    print(solution.longestPalindrome("ababsgfswgesegse"))
